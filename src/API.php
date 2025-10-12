42d79b4519c5615417bb32cf6733fbb4c3a3e1d7
Paul Gossen <paul@gosuccess.io>
2025-10-12 11:27:17 +0200

    refactor: modernize main API class with strict_types and readonly properties
    - Add declare(strict_types=1)
    - Make all controller properties readonly
    - Rename snake_case properties to camelCase (buy_url -> buyUrl, api_setup -> apiSetup)
    - Rename methods to camelCase (get_errors -> getErrors, get_last_error -> getLastError)
    - Add proper PHPDoc with type hints
    - Improve error handling return types

--- src/API.php
+++ src/API.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GoSuccess\Digistore24;
 
 use Exception;
@@ -21,70 +23,81 @@ use GoSuccess\Digistore24\Controllers\RebillingController;
 
 class API
 {
+    /** @var array<int, DigistoreApiException|Exception|string> */
     private array $errors = [];
-    private DigistoreApi $api;
-    public MonitoringController $monitoring;
-    public ProductController $product;
-    public AffiliateController $affiliate;
-    public BuyerController $buyer;
-    public PurchaseController $purchase;
-    public UserController $user;
-    public VoucherController $voucher;
-    public IPNController $ipn;
-    public BillingController $billing;
-    public BuyUrlController $buy_url;
-    public APIController $api_setup;
-    public RebillingController $rebilling;
+    
+    private readonly DigistoreApi $api;
+    
+    public readonly MonitoringController $monitoring;
+    public readonly ProductController $product;
+    public readonly AffiliateController $affiliate;
+    public readonly BuyerController $buyer;
+    public readonly PurchaseController $purchase;
+    public readonly UserController $user;
+    public readonly VoucherController $voucher;
+    public readonly IPNController $ipn;
+    public readonly BillingController $billing;
+    public readonly BuyUrlController $buyUrl;
+    public readonly APIController $apiSetup;
+    public readonly RebillingController $rebilling;
+    public readonly CountryController $country;
 
-    public CountryController $country;
-
-    public function __construct( string $api_key )
+    public function __construct(string $apiKey)
     {
-        try
-        {
-            $this->api = DigistoreApi::connect( $api_key );
-            $this->monitoring = new MonitoringController( $this );
-            $this->product = new ProductController( $this );
-            $this->affiliate = new AffiliateController( $this );
-            $this->buyer = new BuyerController( $this );
-            $this->purchase = new PurchaseController( $this );
-            $this->user = new UserController( $this );
-            $this->voucher = new VoucherController( $this );
-            $this->ipn = new IPNController( $this );
-            $this->billing = new BillingController( $this );
-            $this->buy_url = new BuyUrlController( $this );
-            $this->country = new CountryController( $this );
-            $this->api_setup = new APIController( $this );
-            $this->rebilling = new RebillingController( $this );
-        }
-        catch( DigistoreApiException|Exception $e )
-        {
+        try {
+            $this->api = DigistoreApi::connect($apiKey);
+            $this->monitoring = new MonitoringController($this);
+            $this->product = new ProductController($this);
+            $this->affiliate = new AffiliateController($this);
+            $this->buyer = new BuyerController($this);
+            $this->purchase = new PurchaseController($this);
+            $this->user = new UserController($this);
+            $this->voucher = new VoucherController($this);
+            $this->ipn = new IPNController($this);
+            $this->billing = new BillingController($this);
+            $this->buyUrl = new BuyUrlController($this);
+            $this->country = new CountryController($this);
+            $this->apiSetup = new APIController($this);
+            $this->rebilling = new RebillingController($this);
+        } catch (DigistoreApiException|Exception $e) {
             $this->errors[] = $e;
         }
     }
 
-    public function call( string $action, mixed ...$arguments ): mixed
+    /**
+     * Call an API method dynamically
+     *
+     * @param string $action
+     * @param mixed ...$arguments
+     * @return mixed
+     */
+    public function call(string $action, mixed ...$arguments): mixed
     {
-        try
-        {
-            $result = $this->api->$action( ...$arguments );
-        }
-        catch( DigistoreApiException $e )
-        {
+        try {
+            return $this->api->$action(...$arguments);
+        } catch (DigistoreApiException $e) {
             $this->errors[] = $e->getMessage();
             return null;
         }
-
-        return $result;
     }
 
-    public function get_errors(): array
+    /**
+     * Get all errors that occurred
+     *
+     * @return array<int, DigistoreApiException|Exception|string>
+     */
+    public function getErrors(): array
     {
         return $this->errors;
     }
 
-    public function get_last_error(): DigistoreApiException
+    /**
+     * Get the last error that occurred
+     *
+     * @return DigistoreApiException|Exception|string|false
+     */
+    public function getLastError(): DigistoreApiException|Exception|string|false
     {
-        return end( $this->errors );
+        return end($this->errors);
     }
 }
