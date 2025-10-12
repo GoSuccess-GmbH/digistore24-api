297823bc7911e09eb55585ad509222f5f49805c7
Paul Gossen <paul@gosuccess.io>
2025-10-12 12:58:11 +0200

    chore: remove temporary debug files

--- check-class.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-
-$r = new ReflectionClass('GoSuccess\Digistore24\Util\TypeConverter');
-echo "TypeConverter location: " . $r->getFileName() . PHP_EOL;
-
-$method = $r->getMethod('toArray');
-echo "toArray() method exists: " . ($method ? 'yes' : 'no') . PHP_EOL;
