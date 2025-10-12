53208b2db08b870312e439877b1bfaa906966e83
Paul Gossen <paul@gosuccess.io>
2025-10-12 11:18:04 +0200

    refactor: modernize DigistoreApiException with context support

--- src/Sdk/DigistoreApiException.php
+++ src/Sdk/DigistoreApiException.php
@@ -1,8 +1,36 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GoSuccess\Digistore24\Sdk;
 
 use Exception;
+use Throwable;
 
-class DigistoreApiException extends Exception {
+class DigistoreApiException extends Exception
+{
+    /**
+     * @param string $message
+     * @param int $code
+     * @param array<string, mixed>|null $context Additional context information
+     * @param Throwable|null $previous
+     */
+    public function __construct(
+        string $message,
+        int $code = 0,
+        private readonly ?array $context = null,
+        ?Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * Get additional context information
+     *
+     * @return array<string, mixed>|null
+     */
+    public function getContext(): ?array
+    {
+        return $this->context;
+    }
 }
