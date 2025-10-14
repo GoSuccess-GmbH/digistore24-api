7a120210169dc473ccaf5763bbeba9c214d1c7d8
Paul Gossen <paul@gosuccess.io>
2025-10-14 12:11:47 +0200

    Fix: System Request tests with proper constructor parameters and assertions (2/2)

--- tests/Unit/Request/System/GetGlobalSettingsRequestTest.php
+++ tests/Unit/Request/System/GetGlobalSettingsRequestTest.php
@@ -12,32 +12,35 @@ final class GetGlobalSettingsRequestTest extends TestCase
     public function test_can_create_instance(): void
     {
         $request = new GetGlobalSettingsRequest();
+        
         $this->assertInstanceOf(GetGlobalSettingsRequest::class, $request);
     }
 
-    public function test_endpoint_returns_string(): void
+    public function test_endpoint_returns_correct_value(): void
     {
         $request = new GetGlobalSettingsRequest();
-        $endpoint = $request->getEndpoint();
         
-        $this->assertIsString($endpoint);
-        $this->assertNotEmpty($endpoint);
+        $this->assertSame('/getGlobalSettings', $request->getEndpoint());
     }
 
-    public function test_to_array_returns_array(): void
+    public function test_to_array_returns_empty_array(): void
     {
         $request = new GetGlobalSettingsRequest();
+        
         $array = $request->toArray();
         
         $this->assertIsArray($array);
+        $this->assertEmpty($array);
     }
 
-    public function test_validate_returns_array(): void
+    public function test_validate_returns_empty_array(): void
     {
         $request = new GetGlobalSettingsRequest();
+        
         $errors = $request->validate();
         
         $this->assertIsArray($errors);
+        $this->assertEmpty($errors);
     }
 }
 
--- tests/Unit/Request/System/PingRequestTest.php
+++ tests/Unit/Request/System/PingRequestTest.php
@@ -12,32 +12,35 @@ final class PingRequestTest extends TestCase
     public function test_can_create_instance(): void
     {
         $request = new PingRequest();
+        
         $this->assertInstanceOf(PingRequest::class, $request);
     }
 
-    public function test_endpoint_returns_string(): void
+    public function test_endpoint_returns_correct_value(): void
     {
         $request = new PingRequest();
-        $endpoint = $request->getEndpoint();
         
-        $this->assertIsString($endpoint);
-        $this->assertNotEmpty($endpoint);
+        $this->assertSame('/ping', $request->getEndpoint());
     }
 
-    public function test_to_array_returns_array(): void
+    public function test_to_array_returns_empty_array(): void
     {
         $request = new PingRequest();
+        
         $array = $request->toArray();
         
         $this->assertIsArray($array);
+        $this->assertEmpty($array);
     }
 
-    public function test_validate_returns_array(): void
+    public function test_validate_returns_empty_array(): void
     {
         $request = new PingRequest();
+        
         $errors = $request->validate();
         
         $this->assertIsArray($errors);
+        $this->assertEmpty($errors);
     }
 }
 
