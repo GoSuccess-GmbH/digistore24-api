9a5859e27f73d25efe0461834ace678c8f454509
Paul Gossen <paul@gosuccess.io>
2025-10-14 12:48:59 +0200

    Fix: CustomForm Response tests with proper test data and rawResponse property access (1/1)

--- tests/Unit/Response/CustomForm/ListCustomFormRecordsResponseTest.php
+++ tests/Unit/Response/CustomForm/ListCustomFormRecordsResponseTest.php
@@ -12,17 +12,44 @@ final class ListCustomFormRecordsResponseTest extends TestCase
 {
     public function test_can_create_from_array(): void
     {
-        $data = [];
+        $data = [
+            'records' => [
+                [
+                    'form_id' => 1,
+                    'id' => 100,
+                    'purchase_id' => 'P123',
+                    'purchase_item_id' => 10,
+                    'product_id' => 12345,
+                    'form_no' => 1,
+                    'form_count' => 1,
+                    'data' => ['field1' => 'value1'],
+                    'address' => ['city' => 'Berlin']
+                ]
+            ]
+        ];
         $response = ListCustomFormRecordsResponse::fromArray($data);
         
         $this->assertInstanceOf(ListCustomFormRecordsResponse::class, $response);
+        $this->assertCount(1, $response->getRecords());
     }
 
     public function test_can_create_from_response(): void
     {
         $httpResponse = new Response(
             statusCode: 200,
-            data: ['data' => []],
+            data: [
+                'records' => [
+                    [
+                        'form_id' => 1,
+                        'id' => 100,
+                        'purchase_id' => 'P123',
+                        'purchase_item_id' => 10,
+                        'product_id' => 12345,
+                        'form_no' => 1,
+                        'form_count' => 1
+                    ]
+                ]
+            ],
             headers: [],
             rawBody: ''
         );
@@ -30,20 +57,21 @@ final class ListCustomFormRecordsResponseTest extends TestCase
         $response = ListCustomFormRecordsResponse::fromResponse($httpResponse);
         
         $this->assertInstanceOf(ListCustomFormRecordsResponse::class, $response);
+        $this->assertCount(1, $response->getRecords());
     }
 
     public function test_has_raw_response(): void
     {
         $httpResponse = new Response(
             statusCode: 200,
-            data: ['data' => []],
+            data: [],
             headers: [],
             rawBody: 'test'
         );
         
         $response = ListCustomFormRecordsResponse::fromResponse($httpResponse);
         
-        $this->assertInstanceOf(Response::class, $response->getRawResponse());
+        $this->assertInstanceOf(Response::class, $response->rawResponse);
     }
 }
 
