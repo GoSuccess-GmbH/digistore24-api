9e3c19d7aa3e46168d175f2297eb87b4ce71f32b
Paul Gossen <paul@gosuccess.io>
2025-10-14 12:06:44 +0200

    Fix: SmartUpgrade Request tests with proper constructor parameters and assertions (2/2)

--- tests/Unit/Request/SmartUpgrade/GetSmartupgradeRequestTest.php
+++ tests/Unit/Request/SmartUpgrade/GetSmartupgradeRequestTest.php
@@ -11,33 +11,37 @@ final class GetSmartupgradeRequestTest extends TestCase
 {
     public function test_can_create_instance(): void
     {
-        $request = new GetSmartupgradeRequest();
+        $request = new GetSmartupgradeRequest(smartupgradeId: 'SU123');
+        
         $this->assertInstanceOf(GetSmartupgradeRequest::class, $request);
     }
 
-    public function test_endpoint_returns_string(): void
+    public function test_endpoint_returns_correct_value(): void
     {
-        $request = new GetSmartupgradeRequest();
-        $endpoint = $request->getEndpoint();
+        $request = new GetSmartupgradeRequest(smartupgradeId: 'SU123');
         
-        $this->assertIsString($endpoint);
-        $this->assertNotEmpty($endpoint);
+        $this->assertSame('getSmartupgrade', $request->getEndpoint());
     }
 
-    public function test_to_array_returns_array(): void
+    public function test_to_array_includes_smartupgrade_id(): void
     {
-        $request = new GetSmartupgradeRequest();
+        $request = new GetSmartupgradeRequest(smartupgradeId: 'SU123', purchaseId: 'P12345');
+        
         $array = $request->toArray();
         
         $this->assertIsArray($array);
+        $this->assertSame('SU123', $array['smartupgrade_id']);
+        $this->assertSame('P12345', $array['purchase_id']);
     }
 
-    public function test_validate_returns_array(): void
+    public function test_validate_returns_empty_array(): void
     {
-        $request = new GetSmartupgradeRequest();
+        $request = new GetSmartupgradeRequest(smartupgradeId: 'SU123');
+        
         $errors = $request->validate();
         
         $this->assertIsArray($errors);
+        $this->assertEmpty($errors);
     }
 }
 
--- tests/Unit/Request/SmartUpgrade/ListSmartUpgradesRequestTest.php
+++ tests/Unit/Request/SmartUpgrade/ListSmartUpgradesRequestTest.php
@@ -12,32 +12,35 @@ final class ListSmartUpgradesRequestTest extends TestCase
     public function test_can_create_instance(): void
     {
         $request = new ListSmartUpgradesRequest();
+        
         $this->assertInstanceOf(ListSmartUpgradesRequest::class, $request);
     }
 
-    public function test_endpoint_returns_string(): void
+    public function test_endpoint_returns_correct_value(): void
     {
         $request = new ListSmartUpgradesRequest();
-        $endpoint = $request->getEndpoint();
         
-        $this->assertIsString($endpoint);
-        $this->assertNotEmpty($endpoint);
+        $this->assertSame('listSmartUpgrades', $request->getEndpoint());
     }
 
-    public function test_to_array_returns_array(): void
+    public function test_to_array_returns_empty_array(): void
     {
         $request = new ListSmartUpgradesRequest();
+        
         $array = $request->toArray();
         
         $this->assertIsArray($array);
+        $this->assertEmpty($array);
     }
 
-    public function test_validate_returns_array(): void
+    public function test_validate_returns_empty_array(): void
     {
         $request = new ListSmartUpgradesRequest();
+        
         $errors = $request->validate();
         
         $this->assertIsArray($errors);
+        $this->assertEmpty($errors);
     }
 }
 
