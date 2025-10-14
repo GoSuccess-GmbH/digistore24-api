16a32df19487b4b2643716dd312f2a58e474cd93
Paul Gossen <paul@gosuccess.io>
2025-10-14 11:40:37 +0200

    Fix: Invoice Request tests with proper constructor parameters and assertions (2/2)

--- tests/Unit/Request/Invoice/ListInvoicesRequestTest.php
+++ tests/Unit/Request/Invoice/ListInvoicesRequestTest.php
@@ -11,33 +11,36 @@ final class ListInvoicesRequestTest extends TestCase
 {
     public function test_can_create_instance(): void
     {
-        $request = new ListInvoicesRequest();
+        $request = new ListInvoicesRequest(purchaseId: 'P12345');
+        
         $this->assertInstanceOf(ListInvoicesRequest::class, $request);
     }
 
-    public function test_endpoint_returns_string(): void
+    public function test_endpoint_returns_correct_value(): void
     {
-        $request = new ListInvoicesRequest();
-        $endpoint = $request->getEndpoint();
+        $request = new ListInvoicesRequest(purchaseId: 'P12345');
         
-        $this->assertIsString($endpoint);
-        $this->assertNotEmpty($endpoint);
+        $this->assertSame('listInvoices', $request->getEndpoint());
     }
 
-    public function test_to_array_returns_array(): void
+    public function test_to_array_includes_purchase_id(): void
     {
-        $request = new ListInvoicesRequest();
+        $request = new ListInvoicesRequest(purchaseId: 'P12345');
+        
         $array = $request->toArray();
         
         $this->assertIsArray($array);
+        $this->assertSame('P12345', $array['purchase_id']);
     }
 
-    public function test_validate_returns_array(): void
+    public function test_validate_returns_empty_array(): void
     {
-        $request = new ListInvoicesRequest();
+        $request = new ListInvoicesRequest(purchaseId: 'P12345');
+        
         $errors = $request->validate();
         
         $this->assertIsArray($errors);
+        $this->assertEmpty($errors);
     }
 }
 
