8099f291d211bafd9f22527fa40621f6b4811b05
Paul Gossen <paul@gosuccess.io>
2025-10-15 04:46:23 +0200

    Fix: Payout Response tests with proper test data and rawResponse property access (2/2)

--- tests/Unit/Response/Payout/ListPayoutsResponseTest.php
+++ tests/Unit/Response/Payout/ListPayoutsResponseTest.php
@@ -12,17 +12,42 @@ final class ListPayoutsResponseTest extends TestCase
 {
     public function test_can_create_from_array(): void
     {
-        $data = [];
+        $data = [
+            'data' => [
+                'payout_list' => [
+                    [
+                        'payout_id' => 'PO123',
+                        'amount' => 1500.00,
+                        'date' => '2024-01-15'
+                    ],
+                    [
+                        'payout_id' => 'PO456',
+                        'amount' => 2500.00,
+                        'date' => '2024-02-15'
+                    ]
+                ]
+            ]
+        ];
         $response = ListPayoutsResponse::fromArray($data);
         
         $this->assertInstanceOf(ListPayoutsResponse::class, $response);
+        $this->assertCount(2, $response->getPayoutList());
     }
 
     public function test_can_create_from_response(): void
     {
         $httpResponse = new Response(
             statusCode: 200,
-            data: ['data' => []],
+            data: [
+                'data' => [
+                    'payout_list' => [
+                        [
+                            'payout_id' => 'PO789',
+                            'amount' => 3000.00
+                        ]
+                    ]
+                ]
+            ],
             headers: [],
             rawBody: ''
         );
@@ -30,20 +55,21 @@ final class ListPayoutsResponseTest extends TestCase
         $response = ListPayoutsResponse::fromResponse($httpResponse);
         
         $this->assertInstanceOf(ListPayoutsResponse::class, $response);
+        $this->assertCount(1, $response->getPayoutList());
     }
 
     public function test_has_raw_response(): void
     {
         $httpResponse = new Response(
             statusCode: 200,
-            data: ['data' => []],
+            data: [],
             headers: [],
             rawBody: 'test'
         );
         
         $response = ListPayoutsResponse::fromResponse($httpResponse);
         
-        $this->assertInstanceOf(Response::class, $response->getRawResponse());
+        $this->assertInstanceOf(Response::class, $response->rawResponse);
     }
 }
 
--- tests/Unit/Response/Payout/StatsExpectedPayoutsResponseTest.php
+++ tests/Unit/Response/Payout/StatsExpectedPayoutsResponseTest.php
@@ -12,17 +12,30 @@ final class StatsExpectedPayoutsResponseTest extends TestCase
 {
     public function test_can_create_from_array(): void
     {
-        $data = [];
+        $data = [
+            'data' => [
+                'total_expected' => 5000.00,
+                'payout_count' => 15,
+                'next_payout_date' => '2024-02-01'
+            ]
+        ];
         $response = StatsExpectedPayoutsResponse::fromArray($data);
         
         $this->assertInstanceOf(StatsExpectedPayoutsResponse::class, $response);
+        $this->assertArrayHasKey('total_expected', $response->getData());
+        $this->assertSame(5000.00, $response->getData()['total_expected']);
     }
 
     public function test_can_create_from_response(): void
     {
         $httpResponse = new Response(
             statusCode: 200,
-            data: ['data' => []],
+            data: [
+                'data' => [
+                    'total_expected' => 7500.00,
+                    'payout_count' => 20
+                ]
+            ],
             headers: [],
             rawBody: ''
         );
@@ -30,20 +43,21 @@ final class StatsExpectedPayoutsResponseTest extends TestCase
         $response = StatsExpectedPayoutsResponse::fromResponse($httpResponse);
         
         $this->assertInstanceOf(StatsExpectedPayoutsResponse::class, $response);
+        $this->assertSame(7500.00, $response->getData()['total_expected']);
     }
 
     public function test_has_raw_response(): void
     {
         $httpResponse = new Response(
             statusCode: 200,
-            data: ['data' => []],
+            data: [],
             headers: [],
             rawBody: 'test'
         );
         
         $response = StatsExpectedPayoutsResponse::fromResponse($httpResponse);
         
-        $this->assertInstanceOf(Response::class, $response->getRawResponse());
+        $this->assertInstanceOf(Response::class, $response->rawResponse);
     }
 }
 
