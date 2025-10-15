71633b01fc905d05579208b7190af08aa7f05399
Paul Gossen <paul@gosuccess.io>
2025-10-15 06:43:37 +0200

    refactor: Add PHPDoc to Buyer Request files (3 files)

--- src/Request/Buyer/GetBuyerRequest.php
+++ src/Request/Buyer/GetBuyerRequest.php
@@ -1,12 +1,39 @@
 <?php
+
 declare(strict_types=1);
+
 namespace GoSuccess\Digistore24\Api\Request\Buyer;
+
 use GoSuccess\Digistore24\Api\Base\AbstractRequest;
 use GoSuccess\Digistore24\Api\Http\Method;
+
+/**
+ * Get Buyer Request
+ *
+ * Retrieves buyer information by buyer ID or email address.
+ */
 final class GetBuyerRequest extends AbstractRequest
 {
-    public function __construct(private string $buyerId) {}
-    public function getEndpoint(): string { return 'getBuyer'; }
-    public function method(): Method { return Method::GET; }
-    public function toArray(): array { return ['buyer_id' => $this->buyerId]; }
+    /**
+     * @param string $buyerId The buyer ID or email address
+     */
+    public function __construct(
+        private string $buyerId
+    ) {
+    }
+
+    public function getEndpoint(): string
+    {
+        return 'getBuyer';
+    }
+
+    public function method(): Method
+    {
+        return Method::GET;
+    }
+
+    public function toArray(): array
+    {
+        return ['buyer_id' => $this->buyerId];
+    }
 }
--- src/Request/Buyer/ListBuyersRequest.php
+++ src/Request/Buyer/ListBuyersRequest.php
@@ -1,21 +1,48 @@
 <?php
+
 declare(strict_types=1);
+
 namespace GoSuccess\Digistore24\Api\Request\Buyer;
+
 use GoSuccess\Digistore24\Api\Base\AbstractRequest;
 use GoSuccess\Digistore24\Api\Http\Method;
+
+/**
+ * List Buyers Request
+ *
+ * Retrieves a paginated list of all buyers.
+ */
 final class ListBuyersRequest extends AbstractRequest
 {
+    /**
+     * @param int|null $pageNo Page number (starts at 1)
+     * @param int|null $pageSize Number of items per page
+     */
     public function __construct(
         private ?int $pageNo = null,
         private ?int $pageSize = null,
-    ) {}
-    public function getEndpoint(): string { return 'listBuyers'; }
-    public function method(): Method { return Method::GET; }
+    ) {
+    }
+
+    public function getEndpoint(): string
+    {
+        return 'listBuyers';
+    }
+
+    public function method(): Method
+    {
+        return Method::GET;
+    }
+
     public function toArray(): array
     {
         $params = [];
-        if ($this->pageNo !== null) $params['page_no'] = $this->pageNo;
-        if ($this->pageSize !== null) $params['page_size'] = $this->pageSize;
+        if ($this->pageNo !== null) {
+            $params['page_no'] = $this->pageNo;
+        }
+        if ($this->pageSize !== null) {
+            $params['page_size'] = $this->pageSize;
+        }
         return $params;
     }
 }
