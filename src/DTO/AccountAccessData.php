c6cd89a03ac5d94f882cf505b8aab6846bef970b
Paul Gossen <paul@gosuccess.io>
2025-10-20 16:25:18 +0200

    Remove unnecessary constructors and fromArray from AccountAccessData and RebillingData
    - AbstractDataTransferObject provides automatic fromArray via Reflection
    - Removed manual constructors violating laws.txt
    - Added property defaults for bool properties
    - Removed unused TypeConverter imports
    - All tests still passing
    - PHPStan Level 9 clean

--- src/DTO/AccountAccessData.php
+++ src/DTO/AccountAccessData.php
@@ -6,7 +6,6 @@ namespace GoSuccess\Digistore24\Api\DTO;
 
 use DateTimeImmutable;
 use GoSuccess\Digistore24\Api\Base\AbstractDataTransferObject;
-use GoSuccess\Digistore24\Api\Util\TypeConverter;
 
 /**
  * Account Access Data Transfer Object
@@ -44,118 +43,51 @@ final class AccountAccessData extends AbstractDataTransferObject
         get => $this->modifiedAt;
     }
 
-    public bool $canSeeNonAffiliatePurchases {
+    public bool $canSeeNonAffiliatePurchases = false {
         get => $this->canSeeNonAffiliatePurchases;
     }
 
-    public bool $canApproveAffiliations {
+    public bool $canApproveAffiliations = false {
         get => $this->canApproveAffiliations;
     }
 
-    public bool $canSeeEditMarketplaceLink {
+    public bool $canSeeEditMarketplaceLink = false {
         get => $this->canSeeEditMarketplaceLink;
     }
 
-    public bool $canEditProducts {
+    public bool $canEditProducts = false {
         get => $this->canEditProducts;
     }
 
-    public bool $canEditAffiliateCommissions {
+    public bool $canEditAffiliateCommissions = false {
         get => $this->canEditAffiliateCommissions;
     }
 
-    public bool $canReadMailHistory {
+    public bool $canReadMailHistory = false {
         get => $this->canReadMailHistory;
     }
 
-    public bool $canApprovePurchases {
+    public bool $canApprovePurchases = false {
         get => $this->canApprovePurchases;
     }
 
-    public bool $canEditPurchasesApprovalPolicy {
+    public bool $canEditPurchasesApprovalPolicy = false {
         get => $this->canEditPurchasesApprovalPolicy;
     }
 
-    public bool $canGivePermissions {
+    public bool $canGivePermissions = false {
         get => $this->canGivePermissions;
     }
 
-    public bool $canSeeRevenue {
+    public bool $canSeeRevenue = false {
         get => $this->canSeeRevenue;
     }
 
-    public bool $canEditDiscountVouchers {
+    public bool $canEditDiscountVouchers = false {
         get => $this->canEditDiscountVouchers;
     }
 
-    public bool $canCsvExport {
+    public bool $canCsvExport = false {
         get => $this->canCsvExport;
-    }
-
-    public function __construct(
-        ?int $id = null,
-        ?int $ownerId = null,
-        ?int $accessorId = null,
-        ?string $permissions = null,
-        ?string $permissionsMsg = null,
-        ?DateTimeImmutable $createdAt = null,
-        ?DateTimeImmutable $modifiedAt = null,
-        bool $canSeeNonAffiliatePurchases = false,
-        bool $canApproveAffiliations = false,
-        bool $canSeeEditMarketplaceLink = false,
-        bool $canEditProducts = false,
-        bool $canEditAffiliateCommissions = false,
-        bool $canReadMailHistory = false,
-        bool $canApprovePurchases = false,
-        bool $canEditPurchasesApprovalPolicy = false,
-        bool $canGivePermissions = false,
-        bool $canSeeRevenue = false,
-        bool $canEditDiscountVouchers = false,
-        bool $canCsvExport = false,
-    ) {
-        $this->id = $id;
-        $this->ownerId = $ownerId;
-        $this->accessorId = $accessorId;
-        $this->permissions = $permissions;
-        $this->permissionsMsg = $permissionsMsg;
-        $this->createdAt = $createdAt;
-        $this->modifiedAt = $modifiedAt;
-        $this->canSeeNonAffiliatePurchases = $canSeeNonAffiliatePurchases;
-        $this->canApproveAffiliations = $canApproveAffiliations;
-        $this->canSeeEditMarketplaceLink = $canSeeEditMarketplaceLink;
-        $this->canEditProducts = $canEditProducts;
-        $this->canEditAffiliateCommissions = $canEditAffiliateCommissions;
-        $this->canReadMailHistory = $canReadMailHistory;
-        $this->canApprovePurchases = $canApprovePurchases;
-        $this->canEditPurchasesApprovalPolicy = $canEditPurchasesApprovalPolicy;
-        $this->canGivePermissions = $canGivePermissions;
-        $this->canSeeRevenue = $canSeeRevenue;
-        $this->canEditDiscountVouchers = $canEditDiscountVouchers;
-        $this->canCsvExport = $canCsvExport;
-    }
-
-    public static function fromArray(array $data): static
-    {
-        return new self(
-            id: TypeConverter::toInt($data['id'] ?? null),
-            ownerId: TypeConverter::toInt($data['owner_id'] ?? null),
-            accessorId: TypeConverter::toInt($data['accessor_id'] ?? null),
-            permissions: TypeConverter::toString($data['permissions'] ?? null),
-            permissionsMsg: TypeConverter::toString($data['permissions_msg'] ?? null),
-            createdAt: TypeConverter::toDateTime($data['created_at'] ?? null),
-            modifiedAt: TypeConverter::toDateTime($data['modified_at'] ?? null),
-            canSeeNonAffiliatePurchases: TypeConverter::toBool($data['can_see_non_affiliate_purchases'] ?? null) ?? false,
-            canApproveAffiliations: TypeConverter::toBool($data['can_approve_affiliations'] ?? null) ?? false,
-            canSeeEditMarketplaceLink: TypeConverter::toBool($data['can_see_edit_marketplace_link'] ?? null) ?? false,
-            canEditProducts: TypeConverter::toBool($data['can_edit_products'] ?? null) ?? false,
-            canEditAffiliateCommissions: TypeConverter::toBool($data['can_edit_affiliate_commissions'] ?? null) ?? false,
-            canReadMailHistory: TypeConverter::toBool($data['can_read_mail_history'] ?? null) ?? false,
-            canApprovePurchases: TypeConverter::toBool($data['can_approve_purchases'] ?? null) ?? false,
-            canEditPurchasesApprovalPolicy: TypeConverter::toBool($data['can_edit_purchases_approval_policy'] ?? null) ?? false,
-            canGivePermissions: TypeConverter::toBool($data['can_give_permissions'] ?? null) ?? false,
-            canSeeRevenue: TypeConverter::toBool($data['can_see_revenue'] ?? null) ?? false,
-            canEditDiscountVouchers: TypeConverter::toBool($data['can_edit_discount_vouchers'] ?? null) ?? false,
-            canCsvExport: TypeConverter::toBool($data['can_csv_export'] ?? null) ?? false,
-        );
     }
 }
--- src/DTO/RebillingData.php
+++ src/DTO/RebillingData.php
@@ -6,7 +6,6 @@ namespace GoSuccess\Digistore24\Api\DTO;
 
 use DateTimeImmutable;
 use GoSuccess\Digistore24\Api\Base\AbstractDataTransferObject;
-use GoSuccess\Digistore24\Api\Util\TypeConverter;
 
 /**
  * Rebilling Data Transfer Object
@@ -83,50 +82,5 @@ final class RebillingData extends AbstractDataTransferObject
      */
     public ?string $canCancelBefore = null {
         get => $this->canCancelBefore;
-    }
-
-    public function __construct(
-        bool $modified = false,
-        ?string $note = null,
-        ?string $code = null,
-        ?string $billingStatus = null,
-        ?string $billingStatusMsg = null,
-        ?DateTimeImmutable $nextPaymentAt = null,
-        bool $rebillingActive = false,
-        ?bool $isCancelledNow = null,
-        ?bool $isCancelledLater = null,
-        ?string $canCancelBefore = null,
-    ) {
-        $this->modified = $modified;
-        $this->note = $note;
-        $this->code = $code;
-        $this->billingStatus = $billingStatus;
-        $this->billingStatusMsg = $billingStatusMsg;
-        $this->nextPaymentAt = $nextPaymentAt;
-        $this->rebillingActive = $rebillingActive;
-        $this->isCancelledNow = $isCancelledNow;
-        $this->isCancelledLater = $isCancelledLater;
-        $this->canCancelBefore = $canCancelBefore;
-    }
-
-    /**
-     * Create from API response array
-     *
-     * @param array<string, mixed> $data
-     */
-    public static function fromArray(array $data): static
-    {
-        return new self(
-            modified: TypeConverter::toBool($data['modified'] ?? 'N') ?? false,
-            note: TypeConverter::toString($data['note'] ?? null),
-            code: TypeConverter::toString($data['code'] ?? null),
-            billingStatus: TypeConverter::toString($data['billing_status'] ?? null),
-            billingStatusMsg: TypeConverter::toString($data['billing_status_msg'] ?? null),
-            nextPaymentAt: TypeConverter::toDateTime($data['next_payment_at'] ?? null),
-            rebillingActive: TypeConverter::toBool($data['rebilling_active'] ?? 'N') ?? false,
-            isCancelledNow: TypeConverter::toBool($data['is_cancelled_now'] ?? null),
-            isCancelledLater: TypeConverter::toBool($data['is_cancelled_later'] ?? null),
-            canCancelBefore: TypeConverter::toString($data['can_cancel_before'] ?? null),
-        );
     }
 }
